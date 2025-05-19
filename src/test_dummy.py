39d347e895fca40b249ca912a82195920f8e4210
VeriTas-arch <145042207+VeriTas-arch@users.noreply.github.com>
2025-05-19 10:26:38 +0800

    add dummy viewer

--- src/test_dummy.py
+++ src/test_dummy.py
@@ -7,28 +7,30 @@ import mujoco.viewer
 import numpy as np
 
 # 加载模型
-MODEL_NAME = "inverted_double_pendulum_exp"  # "rotary_inverted_double_pendulum"
+MODEL_NAME = "rotary_inverted_double_pendulum"  # "rotary_inverted_double_pendulum"
+
 ASSET_DIR = f"{Path(__file__).parent.parent}/assets"
 XML_DIR = f"{ASSET_DIR}/{MODEL_NAME}.xml"
-model = mujoco.MjModel.from_xml_path(XML_DIR)
-data = mujoco.MjData(model)
+MODEL = mujoco.MjModel.from_xml_path(XML_DIR)
+DATA = mujoco.MjData(MODEL)
 
 # 初始姿态：轻微扰动
-data.qpos[:] = [0.0, np.pi, 0.0]
+DATA.qpos[:] = [0.0, np.pi, 0.0]
 
 # 控制参数
-step = 0.005
+STEP = 0.005
+IMPULSE_STEP = 0.5 / STEP * 0.05  # 施加冲量，计算方式为 最大扭矩 / 步长 * 比例系数
+
 impulse = 0.0
-impulse_step = 0.5 / step * 0.05  # 施加冲量，计算方式为 最大扭矩 / 步长 * 比例系数
 
 
 def keyboard_callback(key):
     global impulse
     if key == glfw.KEY_LEFT:
-        impulse = impulse_step
+        impulse = IMPULSE_STEP
         print(f"Impulse applied: {impulse}")
     elif key == glfw.KEY_RIGHT:
-        impulse = -impulse_step
+        impulse = -IMPULSE_STEP
         print(f"Impulse applied: {impulse}")
     elif key == glfw.KEY_ESCAPE:
         # 退出程序
@@ -46,10 +48,10 @@ def control_callback(model, data):
 
 # 启动 Viewer（被动模式）
 with mujoco.viewer.launch_passive(
-    model, data, key_callback=keyboard_callback
+    MODEL, DATA, key_callback=keyboard_callback
 ) as viewer:
     while viewer.is_running():
-        control_callback(model, data)
-        mujoco.mj_step(model, data)
+        control_callback(MODEL, DATA)
+        mujoco.mj_step(MODEL, DATA)
         viewer.sync()
-        time.sleep(step)
+        time.sleep(STEP)
---
+++ src/test_generator.py
@@ -0,0 +1,31 @@
+import mujoco
+import mujoco.viewer
+import numpy as np
+from pathlib import Path
+import time
+
+# 加载模型
+MODEL_NAME = "rotary_inverted_double_pendulum"  # "rotary_inverted_double_pendulum"
+
+ASSET_DIR = f"{Path(__file__).parent.parent}/assets"
+XML_DIR = f"{ASSET_DIR}/{MODEL_NAME}.xml"
+MODEL = mujoco.MjModel.from_xml_path(XML_DIR)
+
+data = mujoco.MjData(MODEL)
+
+# 模拟的状态序列（假设提前准备好的 N×nq 的位置序列）
+T = 1000  # 时间步数
+STEP = 0.005
+
+qpos_trajectory = np.zeros((T, 3))  # shape: (T, model.nq)
+qpos_trajectory[:, 0] = np.linspace(-np.pi, np.pi, T)  # qpos[0]
+qpos_trajectory[:, 1] = np.linspace(np.pi + np.pi / 3, np.pi - np.pi / 3, T)  # qpos[1]
+qpos_trajectory[:, 2] = np.linspace(-np.pi / 4, np.pi / 4, T)  # qpos[2]
+
+# 创建渲染器
+with mujoco.viewer.launch_passive(MODEL, data) as viewer:
+    for qpos in qpos_trajectory:
+        data.qpos[:] = qpos
+        mujoco.mj_forward(MODEL, data)  # 更新场景
+        viewer.sync()
+        time.sleep(STEP)
