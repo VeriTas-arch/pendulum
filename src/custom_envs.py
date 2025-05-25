7a1d5f184f41eb5864b20ae73aa263833e25a4d8
VeriTas-arch <145042207+VeriTas-arch@users.noreply.github.com>
2025-05-25 20:05:06 +0800

    chore: auto format code [skip-post-commit]

--- src/custom_envs.py
+++ src/custom_envs.py
@@ -1,11 +1,12 @@
 import logging
 import random
 from pathlib import Path
-import mujoco
 
+import mujoco
 import numpy as np
 from gymnasium import spaces
-from gymnasium.envs.mujoco.inverted_double_pendulum_v5 import InvertedDoublePendulumEnv
+from gymnasium.envs.mujoco.inverted_double_pendulum_v5 import \
+    InvertedDoublePendulumEnv
 
 ASSET_DIR = f"{Path(__file__).parent.parent}/assets"
 DIP_XML_DIR = f"{ASSET_DIR}/inverted_double_pendulum.xml"
