e38c71dadd1763012473e2ebed86a0d24a0e4a24
veritas <veritas-k@qq.com>
2025-05-29 07:48:48 +0000

    test train. temp

--- src/custom_envs.py
+++ src/custom_envs.py
@@ -106,17 +106,17 @@ class CustomRotaryInvertedDoublePendulumEnv(InvertedDoublePendulumEnv):
 
         self.vel_history.clear()
 
-        # angle_offset = np.pi / 36
-        # sign = random.choice([-1, 1])
+        angle_offset = np.pi / 36
+        sign = random.choice([-1, 1])
         if self.mode == "test":
             self.init_qpos = np.array([0.0, np.pi, 0.0])
         elif self.mode == "stable":
-            # self.init_qpos = np.array([0.0, 0.0, 0.0])
+            self.init_qpos = np.array([0.0, 0.0, 0.0])
 
             # init with a small angle offset
-            self.init_qpos = np.array([0.0, 0.13, -0.52])
+            # self.init_qpos = np.array([0.0, 0.13, -0.52])
 
-            # self.init_qpos = np.array([0.0, sign * angle_offset, 0])
+            self.init_qpos = np.array([0.0, sign * angle_offset, 0])
             # amp = 1.0
             # self.init_qvel = np.array([0.0, -0.8 * amp, 0.5 * amp])
         else:
@@ -153,11 +153,12 @@ class CustomRotaryInvertedDoublePendulumEnv(InvertedDoublePendulumEnv):
 
             # 临时设置 stable 模式也不终止
             # terminated = False
-            # reward, reward_info = self._get_rew(x, y, terminated)
-            reward, reward_info = self.compute_reward_stable_test_1(x, y, terminated)
+            reward, reward_info = self._get_rew(x, y, terminated)
+            # reward, reward_info = self.compute_reward_stable_test_2(x, y, terminated)
         elif self.mode == "test":
             terminated = False
-            reward, reward_info = self.compute_reward_test_4(x, y, terminated)
+            # reward, reward_info = self.compute_reward_test_4(x, y, terminated)
+            reward, reward_info = self.compute_reward_swingup_to_stabilize(x, y, terminated)
 
         info = reward_info
 
@@ -668,6 +669,113 @@ class CustomRotaryInvertedDoublePendulumEnv(InvertedDoublePendulumEnv):
             "posture_reward": posture_reward,
             "uprightness": uprightness,
             "angle_diff": angle_diff,
+        }
+
+        return reward, reward_info
+
+    def compute_reward_swingup_to_stabilize(self, x, y, terminated):
+        # 完全竖直且速度为0时的奖励：16.619
+        # --- 获取状态变量 ---
+        theta0, theta1, theta2 = self.data.qpos
+        v0, v1, v2 = self.data.qvel
+        ctrl = (
+            self.data.ctrl[0]
+            if isinstance(self.data.ctrl, np.ndarray)
+            else self.data.ctrl
+        )
+        ctrl = np.array(ctrl)
+
+        # --- 常量 ---
+        x_goal, y_goal = 0.2159, 0.5365
+        target_y = y_goal
+        shift = 0.0
+
+        # --- 奖励项初始化 ---
+        swing_reward = 0.0
+        posture_reward = 0.0
+        alive_bonus = 0.0
+        peak_slow_bonus = 0.0
+        ctrl_penalty = 0.2 * np.sum(ctrl**2)
+
+        # --- 阶段判断 ---
+        uprightness = theta1**2 + (theta1+theta2)**2
+        angle_diff = abs(theta1 + theta2) + abs(theta1)
+
+        # === 起摆阶段奖励 ===
+        if y < 0.3:
+            if y < -0.32:
+                theta_shift = abs(theta1 - np.pi) + abs(theta2)
+                swing_reward = 1.2 * theta_shift
+
+            if y > 0:
+                swing_reward += 1.5
+
+        # === 中间姿态阶段奖励 ===
+        if y >= 0.3 and y < 0.45:
+            height_bonus = np.exp(-8 * (y - target_y) ** 2) * 3
+            angle_bonus = (
+                np.exp(-3 * abs(theta1 - theta2) - 5 * abs(np.sin(theta1))) * 1.5
+            )
+            posture_reward = height_bonus + angle_bonus
+            ctrl_penalty *= 1.5
+
+        # === 顶端稳摆奖励（完全竖直） ===
+        if y >= 0.45:
+            # 距离目标末端位置的惩罚
+            dist_penalty = 5.0 * ((x - x_goal) ** 2 + (y - y_goal) ** 2)
+
+            # 姿态角惩罚（更接近竖直）
+            angle_penalty = 0.1 * (theta1**2 + (theta1+theta2)**2)
+
+            # 控制惩罚加强
+            ctrl_penalty *= 1.5
+
+            # 顶端低速奖励
+            speed_sum = abs(v1) + abs(v2)
+            peak_slow_bonus = 2.0 * max(1.2 - speed_sum, 0.0)
+
+            # 存活奖励
+            posture_reward = (
+                4.0 * np.exp(-8 * uprightness - 8 * angle_diff)
+                + 4 * y
+            )
+
+            alive_bonus = (posture_reward + 3.0) * int(not terminated)
+        else:
+            dist_penalty = 0.1 * (x - x_goal) ** 2
+            angle_penalty = 0.0
+
+        # === 速度惩罚 ===
+        base_vel_penalty = 7 * v0**2 + 3 * v1**2 + 3 * v2**2
+        height_factor = 0.5 + 0.5 * np.tanh(5 * (y - 0.45))
+        vel_penalty = base_vel_penalty * 7e-3 * height_factor + 0.04 * ctrl_penalty
+
+        # === 总奖励 ===
+        reward = (
+            alive_bonus
+            + swing_reward
+            + peak_slow_bonus
+            + posture_reward
+            - dist_penalty
+            - angle_penalty
+            - vel_penalty
+            - ctrl_penalty
+            + shift
+        )
+
+        # === 信息字典 ===
+        reward_info = {
+            "reward_survive": alive_bonus,
+            "swing_reward": swing_reward,
+            "posture_reward": posture_reward,
+            "distance_penalty": -dist_penalty,
+            "angle_penalty": -angle_penalty,
+            "velocity_penalty": -vel_penalty,
+            "ctrl_penalty": -ctrl_penalty,
+            "peak_slow_bonus": peak_slow_bonus,
+            "uprightness": uprightness,
+            "angle_diff": angle_diff,
+            "y": y,
         }
 
         return reward, reward_info
--- tools/setup.sh
+++
@@ -1,4 +0,0 @@
-git config --global user.email "veritas-k@qq.com"
-git config --global user.name "veritas"
-
-conda activate /home/featurize/work/RL
